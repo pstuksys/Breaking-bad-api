0a3ac274705160a100ec890136f68ee1fc8f14ba
Paul'ius <stuksys.paulius@gmail.com>
2021-11-15 18:50:32 +0200

    Breaking bad

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Header from './components/UI/Header';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/UI/Search';
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [ isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(()=>{
+    const fetchItems = async () =>{
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+    
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  },[query])
+
+
+  return (
+    <div className="container">
+      <Header/>
+      <Search getQuery={(query)=> setQuery(query)}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Search.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('');
+    const onChange = (query) => {
+        setText(query)
+        getQuery(query)
+    }
+
+    return (
+        <section className='search'>
+            <form>
+                <input type='text' className='form-control' placeholder='Search characters' autoFocus
+                    value={text} onChange={(e) => onChange(e.target.value)} />
+            </form>
+        </section>
+    )
+}
+
+export default Search
